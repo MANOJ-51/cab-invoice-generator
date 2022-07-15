b4c0fb625206fea914a48ba15522804147a7026c
MANOJ-51 <m4anoj51@gmail.com>
2022-07-15 19:10:12 +0530

    added: method to get multiple rides and writter test cases

--- com/bridgelabz/CabInvoiceTest.java
+++ com/bridgelabz/CabInvoiceTest.java
@@ -23,5 +23,14 @@ public class CabInvoiceTest {
         double fare = cabInvoiceGenerator.calculateFare(0.1, 1);
         Assert.assertEquals(5, fare, 0.0);
     }
+
+    @Test
+    public void givenMultipleRidesShouldReturnTotalFare (){
+        Ride[] rides= {new Ride (2.0,5),
+                       new Ride(0.1,1)
+                      };
+        double fare =cabInvoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
+    }
 }
 
